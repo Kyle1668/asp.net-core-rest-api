af4913b8666e20c5d9337dbe8b5a17f1f050505f
kyle1668 <kyledevinobrien1@gmail.com>
2018-12-30 15:15:58 -0800

    added getters and setters

---
+++ REST-API-Example/Models/Color.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REST_API_Example.Models
+{
+    public class Color
+    {
+        private readonly string _hex;
+
+        public Color(string inHex)
+        {
+            _hex = inHex;                        
+        }
+
+        public string GetHex()
+        {
+            return _hex;
+        }
+    }
+}
